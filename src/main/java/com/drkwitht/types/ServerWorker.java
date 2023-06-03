e3b064cf8d58360defcf85068b1accc175bab670
drkwitht <derektan3510@gmail.com>
2023-06-03 15:28:48 -0700

    Fixed control logic.

--- src/main/java/com/drkwitht/types/ServerWorker.java
+++ src/main/java/com/drkwitht/types/ServerWorker.java
@@ -3,8 +3,7 @@ package com.drkwitht.types;
 /**
  * ServerWorker.java
  * @author Derek Tan (DrkWithT at GitHub)
- * TODO: 1. create DocResource and ResourceCache classes.
- * TODO: 2. create RequestHandler class and a calling method in ServerWorker.
+ * @apiNote This may be moved into the main server class to easily use a synchronized ResourceCache to save memory.
  */
 
 import java.io.BufferedReader;
@@ -13,49 +12,81 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import javax.net.ssl.SSLSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import com.drkwitht.handlers.HandlerStorage;
+import com.drkwitht.handlers.IRequestHandler;
+import com.drkwitht.handlers.MyHttpResponse;
+import com.drkwitht.handlers.ResourceCache;
 import com.drkwitht.http.HttpReader;
 import com.drkwitht.http.HttpRequest;
+import com.drkwitht.http.HttpStatus;
 
 public class ServerWorker implements Runnable {
+    public static final int MAX_STRIKES = 2;
+
     private WorkerState state;
-    private WorkerError error;
+    private int strikes;
 
-    private String serverString;
-    private int serverMajorVersion;
-    private int serverMinorVersion;
+    private String hostString;
 
-    private SSLSocket workerSocket;
+    private Socket workerSocket;
     private DataInputStream fromClient;
     private DataOutputStream toClient;
 
     private HttpReader messageReader;
-    private String requestURL;
+    private MyHttpResponse messageSender;
+    private ResourceCache resCacheReference;  // NOTE: stores a reference value of main class's ResourceCache!
+    private HandlerStorage handlerTableRef;   // NOTE: stores a reference to server handler collection!
     
-    public ServerWorker(SSLSocket socket, String serverName, int versionCode) throws IOException {
+    private Logger debugLogger;
+
+    public ServerWorker(Socket socket, String hostIdentifier, ResourceCache cacheReference, HandlerStorage handlers) throws IOException {
         state = WorkerState.IDLE;
-        error = WorkerError.NONE;
+        strikes = 0;
         
-        serverString = serverName;
-        serverMajorVersion = (versionCode & 0xff00) >> 8;
-        serverMinorVersion = (versionCode & 0x00ff);
+        hostString = hostIdentifier;
         
         workerSocket = socket;
         fromClient = new DataInputStream(socket.getInputStream());
         toClient = new DataOutputStream(socket.getOutputStream());
 
-        messageReader = null;
-        requestURL = null;
+        messageReader = new HttpReader(new BufferedReader(new InputStreamReader(fromClient)), hostString);
+        messageSender = new MyHttpResponse(toClient);
+        resCacheReference = cacheReference;
+        handlerTableRef = handlers;
+
+        debugLogger = Logger.getLogger("ServerWorker");
+        debugLogger.setLevel(Level.INFO);
+        debugLogger.info("Created instance.");
+    }
+
+    private WorkerState stateRespond(HttpRequest request, HttpStatus optional) throws IOException {
+        IRequestHandler currentHandler = null;
+        
+        if (request != null)
+            currentHandler = handlerTableRef.getHandler(request.getPath());
+
+        debugLogger.info("request.path = " + request.getPath());  // DEBUG
+        
+        currentHandler.handle(resCacheReference, request, messageSender, optional);
+
+        return WorkerState.START;
     }
 
     @Override
     public void run() {
-        messageReader = new HttpReader(new BufferedReader(new InputStreamReader(fromClient)));
-
         HttpRequest tempRequest = null;
 
         while (state != WorkerState.STOP) {
+            if (strikes > MAX_STRIKES) {
+                state = WorkerState.STOP;
+            }
+
+            debugLogger.info("ServerWorker.state = " + state);  // DEBUG!
+
             try {
                 switch (state) {
                     case IDLE:
@@ -66,25 +97,48 @@ public class ServerWorker implements Runnable {
                         break;
                     case REQUEST:
                         tempRequest = messageReader.run();
+                        messageReader.reset();
+                        if (!messageReader.isHostValid()) {
+                            state = WorkerState.ERROR_REQUEST;  // check host header or reject request!
+                        } else {
+                            state = WorkerState.RESPOND;
+                        }
                         break;
                     case ERROR_REQUEST:
+                        state = stateRespond(tempRequest, HttpStatus.BAD_REQUEST);
                         break;
                     case ERROR_NOT_FOUND:
+                        state = stateRespond(tempRequest, HttpStatus.NOT_FOUND);
                         break;
                     case ERROR_GENERIC:
+                        state = stateRespond(tempRequest, HttpStatus.SERVER_ERROR);
                         break;
                     case ERROR_UNSUPPORTED:
+                        state = stateRespond(tempRequest, HttpStatus.NOT_IMPLEMENTED);
                         break;
                     case RESPOND:
-                        // TODO: 3a. create stringifiable HttpResponse before RESPOND state!
+                        state = stateRespond(tempRequest, HttpStatus.NOT_FOUND);
                         break;
                     case STOP:
-                    default:
                         break;
+                    default:
+                        throw new Exception("Invalid worker state.");
                 }
-            } catch (Exception generalErr) {
-                // TODO: 4. a default handler for 500 responses goes here for now!
+            } catch (IOException ioError) {
+                debugLogger.warning(ioError.toString());
+                strikes++;
+                state = WorkerState.ERROR_REQUEST;
+            } catch (Exception generalError) {
+                debugLogger.warning(generalError.toString());
+                strikes++;
+                state = WorkerState.ERROR_GENERIC;
             }
+        }
+
+        try {
+            workerSocket.close();
+        } catch (IOException ioError) {
+            debugLogger.warning(ioError.getMessage());
         }
     }
 }
